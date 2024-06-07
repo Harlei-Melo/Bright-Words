1c759ac2f33fca3e346850b7488e3b030a7586de
Harlei Melo <idzeronee@gmail.com>
2024-06-07 18:13:17 -0300

    Criação do projeto

---
+++ js/script.js
@@ -0,0 +1,75 @@
+document.addEventListener('DOMContentLoaded', function() {
+  let index = 0,
+      interval = 1000;
+
+  const rand = (min, max) => 
+    Math.floor(Math.random() * (max - min + 1)) + min;
+
+  const animate = star => {
+    star.style.setProperty("--star-left", `${rand(-10, 100)}%`);
+    star.style.setProperty("--star-top", `${rand(-40, 80)}%`);
+
+    star.style.animation = "none";
+    star.offsetHeight;
+    star.style.animation = "";
+
+    setTimeout(() => {
+      animate(star);
+    }, 1000);
+  }
+
+  for(const star of document.getElementsByClassName("sparkle-effect")) {
+    setTimeout(() => {
+      animate(star);
+    }, index++ * (interval / 3))
+  }
+
+  const quotes = [
+    { text: "A única coisa que sei é que nada sei.", author: "Sócrates" },
+    { text: "Quando você olha muito tempo para um abismo, o abismo olha para você.", author:"Friedrich Nietzsche" },
+    { text: "Daria tudo que sei pela metade do que ignoro.", author:"René Descartes" },
+    { text: "Dedica-se a   esperar o futuro apenas quem não sabe viver o presente.", author:"Sêneca" },
+  ];
+
+  function changeQuote() {
+    const quoteBox = document.getElementById('quote-box');
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const newQuote = quotes[randomIndex];
+    const quoteText = newQuote.text;
+  
+    // Divida o texto da citação em duas partes aleatórias
+    const effectLength = 23;
+    const firstPart = quoteText.slice(0, effectLength);
+    const secondPart = quoteText.slice(effectLength);
+  
+    quoteBox.style.opacity = 0;
+  
+    setTimeout(() => {
+      // Aplique o efeito de brilho apenas na segunda parte do texto
+      quoteBox.innerHTML = `<h1 id="quote-box">
+      ${firstPart}
+        <span class="sparkle">
+          <span class="sparkle-effect">
+            <img src="svg/shine.svg" onload="SVGInject(this)">
+          </span>
+          <span class="sparkle-effect">
+            <img src="svg/shine.svg" onload="SVGInject(this)">
+          </span>
+          <span class="sparkle-effect">
+            <img src="svg/shine.svg" onload="SVGInject(this)">
+          </span>
+          <span></span><span class="sparkle-text">${secondPart}</span>
+        </span>
+        <br> – ${newQuote.author}</h1>`;
+  
+      // Reaplique a animação aos novos elementos sparkle-effect
+      for (const star of quoteBox.getElementsByClassName("sparkle-effect")) {
+        animate(star);
+      }
+  
+      quoteBox.style.opacity = 1;
+    }, 500);
+  }
+  
+  document.getElementById('change-quote').addEventListener('click', changeQuote);
+});
---
+++ readme.md
@@ -0,0 +1,48 @@
+# Bright Words
+
+"Bright Words" é um projeto simples e elegante que destaca animações e estilos visuais atrativos.
+
+## Descrição
+
+Este projeto exibe uma citação de Platão usando HTML, CSS e JavaScript. A citação é apresentada com animações de estrelas brilhantes ao redor do texto, criando um efeito visual encantador. É ideal para páginas de apresentação, sites pessoais ou qualquer projeto que precise de um toque filosófico e inspirador.
+
+## Captura de Tela
+
+![Bright Words Screenshot](images/gifs/Bright%20words-clip.gif)
+
+## Tecnologias Utilizadas
+
+- HTML
+- CSS (com SCSS para organização de estilos e performance)
+- JavaScript
+- [SVGInject](https://github.com/iconfu/svg-inject) para manipulação de SVGs
+- [Font Awesome](https://fontawesome.com) para ícones
+- [Google Fonts - Merriweather](https://fonts.google.com/specimen/Merriweather) para tipografia
+
+## Estrutura do Projeto
+
+![Estrutura Screenshot](images/Screenshot%202024-06-06%20161839.png)
+
+
+## Contribuição
+
+Sinta-se à vontade para contribuir com este projeto. Para isso:
+
+1. Faça um fork do projeto
+2. Crie uma branch para sua feature:
+    ``` bash
+    git checkout -b minha-nova-feature ```
+3. Commit suas alterações:
+    ``` bash
+    git commit -m 'Adiciona nova feature' ```
+4. Push para a branch:
+    ``` bash
+    git push origin minha-nova-feature ```
+5. Abra um Pull Request
+
+## Referências
+
+- [SVGInject](https://github.com/iconfu/svg-inject)
+- [Font Awesome](https://fontawesome.com)
+- [Google Fonts - Merriweather](https://fonts.google.com/specimen/Merriweather)
+- [Linear](https://linear.app/readme)
