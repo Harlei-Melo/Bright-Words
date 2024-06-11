b90cc3b0684891681fc3b2011c3c4d9977aafacf
Harlei Melo <idzeronee@gmail.com>
2024-06-10 23:05:35 -0300

    feat: reponsividade ajustada

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
     { text: "A única coisa que sei é que nada sei.", author: "Sócrates" },
     { text: "Quando você olha muito tempo para um abismo, o abismo olha para você.", author:"Friedrich Nietzsche" },
     { text: "Daria tudo que sei pela metade do que ignoro.", author:"René Descartes" },
-    { text: "Dedica-se a   esperar o futuro apenas quem não sabe viver o presente.", author:"Sêneca" },
+    { text: "Dedica-se a esperar o futuro apenas quem não sabe viver o presente.", author:"Sêneca" },
   ];
 
   function changeQuote() {
@@ -70,6 +70,30 @@ document.addEventListener('DOMContentLoaded', function() {
       quoteBox.style.opacity = 1;
     }, 500);
   }
-  
+
+  function copyQuote() {
+    const quoteBox = document.getElementById('quote-box');
+    const textToCopy = quoteBox.innerText;
+    navigator.clipboard.writeText(textToCopy).then(() => {
+      showToast("Frase copiada!");
+    }).catch(err => {
+      console.error("Erro ao copiar a frase: ", err);
+    });
+  }
+
+  function showToast(message) {
+    const toastContainer = document.getElementById('toast-container');
+    const toast = document.createElement('div');
+    toast.className = 'toast';
+    toast.innerHTML = `<i class="fa-solid fa-check-circle"></i> ${message}`;
+    toastContainer.appendChild(toast);
+
+    // Remova o toast após a animação
+    setTimeout(() => {
+      toast.remove();
+    }, 3000);
+  }
+
   document.getElementById('change-quote').addEventListener('click', changeQuote);
+  document.getElementById('copy-quote').addEventListener('click', copyQuote);
 });
